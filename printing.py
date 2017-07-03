2e1bd2572514bb8e24cb28c68dfc44a271b93994
Abel Nagy <nagy9219abel@gmail.com>
2017-07-03 15:28:54 +0200

    first commit, 5 passes

--- printing.py
+++ printing.py
@@ -1,2 +1,8 @@
-
 # Printing functions
+import reports
+
+print(reports.count_games('/home/abel/codecool/pbwp-3rd-si-game-statistics-abelNagy/game_stat.txt'))
+print(reports.decide('/home/abel/codecool/pbwp-3rd-si-game-statistics-abelNagy/game_stat.txt', 1999))
+print(reports.get_latest('/home/abel/codecool/pbwp-3rd-si-game-statistics-abelNagy/game_stat.txt'))
+print(reports.count_by_genre('/home/abel/codecool/pbwp-3rd-si-game-statistics-abelNagy/game_stat.txt', 'Real-time strategy'))
+print(reports.get_line_number_by_title('/home/abel/codecool/pbwp-3rd-si-game-statistics-abelNagy/game_stat.txt', 'Counter-Strike'))
--- reports.py
+++ reports.py
@@ -1,2 +1,50 @@
-
 # Report functions
+
+
+def count_games(file_name):
+    with open(file_name) as file:
+        game_count = file.readlines()
+    return len(game_count)
+
+
+def decide(file_name, year):
+    with open(file_name) as file:
+        year = str(year)
+        for line in file:
+            if year in line:
+                return True
+    return False
+
+
+def get_latest(file_name):
+    with open(file_name) as file:
+        latest_game_name = ""
+        latest_game_date = 0
+        for line in file:
+            line = line.split('\t')
+            if int(line[2]) > latest_game_date:
+                latest_game_name = line[0]
+                latest_game_date = int(line[2])
+        return latest_game_name
+
+
+def count_by_genre(file_name, genre=None):
+    with open(file_name) as file:
+        games_by_genre = 0
+        for line in file:
+            line = line.split('\t')
+            if genre == line[3]:
+                games_by_genre += 1
+        return games_by_genre
+
+
+def get_line_number_by_title(file_name, title):
+    with open(file_name) as file:
+        line_number = 0
+        for line in file:
+            line_number += 1
+            line = line.split('\t')
+            if title == line[0]:
+                return line_number
+        else:
+            raise ValueError('No game found with that title')
