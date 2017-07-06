e6d5d719b836deb04daf6aec6a66698524f2b966
Abel Nagy <nagy9219abel@gmail.com>
2017-07-06 15:05:25 +0200

    first commit. first 6 exercises pass in test

--- part2/printing.py
+++ part2/printing.py
@@ -1,2 +1,10 @@
-
 # Printing functions
+
+import reports
+
+print(reports.get_most_played('/home/abel/codecool/pbwp-3rd-si-game-statistics-abelNagy/part2/game_stat.txt'))
+print(reports.sum_sold('/home/abel/codecool/pbwp-3rd-si-game-statistics-abelNagy/part2/game_stat.txt'))
+print(reports.get_selling_avg('/home/abel/codecool/pbwp-3rd-si-game-statistics-abelNagy/part2/game_stat.txt'))
+print(reports.count_longest_title('/home/abel/codecool/pbwp-3rd-si-game-statistics-abelNagy/part2/game_stat.txt'))
+print(reports.get_date_avg('/home/abel/codecool/pbwp-3rd-si-game-statistics-abelNagy/part2/game_stat.txt'))
+print(reports.get_game('/home/abel/codecool/pbwp-3rd-si-game-statistics-abelNagy/part2/game_stat.txt', 'Minecraft'))
--- part2/reports.py
+++ part2/reports.py
@@ -1,2 +1,64 @@
-
 # Report functions
+# My questions: 1. indentation of the return statement?
+# My questions: 2. str.replace() creates a copy. Is there a method that strips the element of a list?
+
+
+def get_most_played(file_name):
+    with open(file_name) as file:
+        most_played = ""
+        sold_copies = 0
+        for line in file:
+            line = line.split('\t')
+            if float(line[1]) > sold_copies:
+                most_played = line[0]
+                sold_copies = float(line[1])
+    return most_played
+
+
+def sum_sold(file_name):
+    with open(file_name) as file:
+        total_sold = 0
+        for line in file:
+            line = line.split('\t')
+            total_sold += float(line[1])
+    return total_sold
+
+
+def get_selling_avg(file_name):
+    with open(file_name) as file:
+        total_sold = sum_sold(file_name)
+        number_of_games = len(file.readlines())
+        selling_avg = total_sold / number_of_games
+    return selling_avg
+
+
+def count_longest_title(file_name):
+    with open(file_name) as file:
+        longest_title = ""
+        for line in file:
+            line = line.split('\t')
+            if len(line[0]) > len(longest_title):
+                longest_title = line[0]
+    return len(longest_title)
+
+
+def get_date_avg(file_name):
+    with open(file_name) as file:
+        number_of_games = []
+        total_years = 0
+        for line in file:
+            line = line.split('\t')
+            number_of_games.append(line[0])
+            total_years += int(line[2])
+    return round(total_years / len(number_of_games))
+
+
+def get_game(file_name, title):
+    with open(file_name) as file:
+        for line in file:
+            line = line.split('\t')
+            if title in line:
+                line[1] = float(line[1])
+                line[2] = int(line[2])
+                line[-1] = line[-1].replace('\n', '')
+                return line
